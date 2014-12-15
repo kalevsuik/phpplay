5d7a13b7bb0dfe03200094789e2bc2a6f2cf7c30
Kalev Suik <kalev.suik@gmail.com>
2014-12-15 16:56:18 +0200

    teine viis arraysse

--- models/FeedBackMsg.class.php
+++ models/FeedBackMsg.class.php
@@ -1,32 +1,16 @@
 <?php
 
+require_once 'functions.php';
+
 class FeedBackMsg
 {
 	public function getAllFeedBack()
 	{
-		/*$pildid = array(
-			array('user' => 'uks','location' => 'Monterey 1', 'feedback' => 'kena kohake 1'),
-			array('user' => 'uks','location' => 'Monterey 2', 'feedback' => 'pole paha'),
-			array('user' => 'kolm','location' => 'Monterey 1', 'feedback' => 'jube urgas'),
-			array('user' => 'kaks','location' => 'Monterey 1', 'feedback' => 'pandav pleiss'),
-			array('user' => 'kaks','location' => 'Surmaorg 1', 'feedback' => 'surm siinv õi siberis'),
-			array('user' => 'neli','location' => 'Surmaorg 2', 'feedback' => 'mehed ming ka'),
-		);
-		return $pildid;*/
 		return db_all_feedback();
 	}
 
 	public function getUserFeedBack($user_id)
 	{
-		/*$pildid = array(
-			array('location' => 'Monterey 1', 'feedback' => 'kena kohake 1'),
-			array('location' => 'Monterey 2', 'feedback' => 'pole paha'),
-			array('location' => 'Monterey 1', 'feedback' => 'jube urgas'),
-			array('location' => 'Monterey 1', 'feedback' => 'pandav pleiss'),
-			array('location' => 'Surmaorg 1', 'feedback' => 'surm siin või siberis'),
-			array('location' => 'Surmaorg 2', 'feedback' => 'mehed ming ka'),
-		);
-		return $pildid;*/
 		return db_user_feedback($user_id);
 	}
 }
